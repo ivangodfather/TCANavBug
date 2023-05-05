74fff44d35889b45a8b9cfa3dc90028ad3a4bf7f
Ivan Ruiz <ruiz@fueled.com>
2023-05-05 11:46:07 +0200

    Initial Commit

---
+++ NewTCAExample/NewTCAExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewTCAExampleApp.swift
+//  NewTCAExample
+//
+//  Created by Iván Ruiz Monjo on 5/5/23.
+//
+
+import SwiftUI
+
+@main
+struct NewTCAExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
